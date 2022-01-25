08abbfe54f6c12920087c10b85cf6e57b4901ae8
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-25 12:27:27 -0800

    wrote three calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { useEffect } from 'react';
+import { getMachines, getVillagers2 } from './services/promise-me';
+import { getVillagers } from './services/promise-me';
+
 export default function App() {
+  useEffect(() => {
+    getVillagers2();
+    getVillagers();
+    getMachines();
+  });
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,15 +1,32 @@
 /**
  * TODO:
  */
-
+export async function getVillagers() {
+  const response = await fetch('https://acnhapi.com/v1/villagers');
+  const data = await response.json();
+  console.log('data', data);
+  return data;
+}
 /**
  * TODO:
  */
-
+export function getVillagers2() {
+  const data = fetch('https://acnhapi.com/v1/villagers')
+    .then((data) => data.json())
+    .then((villagerData) => {
+      console.log('villagerData', villagerData);
+    });
+  return data;
+}
 /**
  * TODO:
  */
-
+export async function getMachines() {
+  const resp = await fetch('https://ponball.herokuapp.com/api/v1/machines');
+  const data = await resp.json();
+  console.log('data', data);
+  return data;
+}
 /**
  * TODO:
  */
